53de160dd36772a3c865dfd9631028a597469626
Mr Timo <hungryhungrytimo@gmail.com>
2012-10-02 15:50:20 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+What2bring::Application.config.secret_token = 'ed9ae792e10b0c34e8a3a0f2c297a82b2d8739360e7106166ee218e238962545328f00a5b3f4fde3d79441cbe9732066794abe34a19d33698687a45b73dea811'
