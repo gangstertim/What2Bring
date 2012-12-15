5a37db43d50bc38cabac8c23e72b4507a8d01b04
Mr Timo <hungryhungrytimo@gmail.com>
2012-12-14 19:44:59 -0500

    added some validation testing

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe Event do
-	it "requires appropriate data" do
+	it "requires only necessary data" do
 		subject.should_not be_valid
 
 		subject.name = "Tim"
-		subject.date = "2012-10-15"
-		subject.location = "My Kitchen"
+		subject.datec = "2012-10-15"
 		subject.email = "hungryhungrytimo@gmail.com"
 		subject.cash = false
+		subject.location = "The neighbor's patio"
 
 		subject.should be_valid
 	end
@@ -16,16 +16,17 @@ end
 
 
 describe Event do
-	it "howmuch is required with cash" do
+	it "has a dumb date" do
 		subject.should_not be_valid
 
 		subject.name = "Tim"
-		subject.date = "2012-10-15"
+		subject.datec = "dead fish"
 		subject.location = "My Kitchen"
 		subject.email = "hungryhungrytimo@gmail.com"
 		subject.cash = true
 
 		subject.should_not be_valid
+
 	end
 end
 
@@ -34,10 +35,28 @@ describe Event do
 		subject.should_not be_valid
 
 		subject.name = "Tim"
-		subject.date = "2012-10-15"
+		subject.datec = "next thrusday at noon"
+		subject.location = "My Kitchen"
+		subject.email = "hungryhungrytimo@gmail.com"
+		subject.cash = false
+		subject.howmuch = 7.25
+
+		subject.should_not be_valid
+
+		subject.cash = true
+	end
+end
+
+describe Event do
+	it "has some really nice dishes" do
+		subject.should_not be_valid
+
+		subject.name = "Tim"
+		subject.datec = "2012-10-15"
 		subject.location = "My Kitchen"
 		subject.email = "hungryhungrytimo@gmail.com"
 		subject.cash = true
+		subject.dishes ="cats, dogs, robots, fried eggs, one large vanille latte"
 		subject.howmuch = 7.25
 
 		subject.should be_valid
